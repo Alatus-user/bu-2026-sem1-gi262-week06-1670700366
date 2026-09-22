37887788332e97efbc6014425882b53e3f15283d
Alatus-user <thirasak26679@gmail.com>
2026-09-22 13:14:20 +0700

    prob_1

--- Assets/Midterm Exam/Prob01-ClassDiagram/GameEntity.cs
+++ Assets/Midterm Exam/Prob01-ClassDiagram/GameEntity.cs
@@ -1,9 +1,31 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace MidtermExam.Prob01
 {
     public class GameEntity
     {
+        public string id;
+        private Vector3 position;
+        protected int health;
+
+
+        public virtual void Update()
+        {
+
+        }
+
+        public virtual void TakeDamage(int damage)
+        {
+
+        }
+
+        private void Move(Vector3 direction)
+        {
+
+        }
+
+
         // TODO: Implement fields and methods according to Class Diagram
     }
 }
